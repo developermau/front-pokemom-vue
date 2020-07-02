a512e9f6b4a5e289ec87344041b078e463495bc3
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2020-07-02 17:40:21 -0400

    stable

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
-      <h1>POKEDEX - "Coursera / 2020"</h1>
+      <h1>POKEDEX - "Coursera"</h1>
     </v-app-bar>
     <v-main>
       <v-container fluid>
--- src/views/Pokemon.vue
+++ src/views/Pokemon.vue
@@ -1,32 +1,36 @@
 <template>
   <v-container fluid class="justify-center">
     <h1 class="text-center">{{pokemon.name | uppercase}}</h1>
-    <v-row>
-      <v-col cols="4" md="4" border>
-        <v-img
-          v-if="pokemon.sprites !== undefined"
-          :src="pokemon.sprites.front_default"
-          aspect-ratio="1"
-          contain
-        ></v-img>
-      </v-col>
-      <v-col cols="8" md="8">
-        <v-list class="transparent">
-          <v-list-item>
-            <v-list-item-content>
-              <v-list-item-title>Weight</v-list-item-title>
-              <v-list-item-subtitle>{{pokemon.weight/10}} kg.</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-content>
-              <v-list-item-title>Height</v-list-item-title>
-              <v-list-item-subtitle>{{pokemon.height*10}} cm.</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-col>
-    </v-row>
+    <v-card elevation="2" outlined>
+      <v-card-text>
+        <v-row class="text-center">
+          <v-col>
+            <v-img
+              v-if="pokemon.sprites !== undefined"
+              :src="pokemon.sprites.front_default"
+              aspect-ratio="1"
+              contain
+            ></v-img>
+          </v-col>
+          <v-col>
+            <v-list class="transparent">
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title>Weight</v-list-item-title>
+                  <v-list-item-subtitle>{{pokemon.weight/10}} kg.</v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title>Height</v-list-item-title>
+                  <v-list-item-subtitle>{{pokemon.height*10}} cm.</v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+          </v-col>
+        </v-row>
+      </v-card-text>
+    </v-card>
   </v-container>
 </template>
 
