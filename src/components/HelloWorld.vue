23ea697ee76c175bf550d01e34dc6879c5fdcdfb
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2020-07-02 01:13:01 -0400

    + Pokemon Card

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,17 +3,8 @@
     <h1>Pokemons</h1>
     <v-container fluid>
       <v-row>
-        <v-col cols="12" sm="6" md="4">
-          <PokemonCard />
-        </v-col>
-        <v-col cols="12" sm="6" md="4">
-          <PokemonCard />
-        </v-col>
-        <v-col cols="12" sm="6" md="4">
-          <PokemonCard />
-        </v-col>
-        <v-col cols="12" sm="6" md="4">
-          <PokemonCard />
+        <v-col cols="12" sm="6" md="4" v-for="(pokemon, index) in pokemonList" :key="index">
+          <PokemonCard :urlPokemon="pokemon.url" />
         </v-col>
       </v-row>
     </v-container>
@@ -21,12 +12,50 @@
 </template>
 
 <script>
+import axios from "axios";
 import PokemonCard from "./PokemonCard";
 
 export default {
   name: "HelloWorld",
   components: {
     PokemonCard
+  },
+  data() {
+    return {
+      pokemonList: []
+    };
+  },
+  created() {
+    const externalService = "https://pokeapi.co/api/v2";
+    const resource = `${externalService}/pokemon`;
+    // Load
+    this.loadPokemons(resource);
+  },
+  methods: {
+    loadPokemons(resource) {
+      console.log("Loading pokemons...");
+
+      console.log(`Querying to resource: ${resource}`);
+
+      // Connect to API: https://pokeapi.co/api/v2
+      axios
+        .get(resource)
+        .then(response => {
+          console.log("\nresponse-->", response);
+          const { data } = response;
+
+          if (data !== null && data !== undefined) {
+            const { results } = data;
+            if (results !== null && results !== undefined) {
+              console.log("results", results);
+              this.pokemonList = results;
+            }
+          }
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
   }
 };
 </script>
--- src/components/PokemonCard.vue
+++ src/components/PokemonCard.vue
@@ -1,19 +1,61 @@
 <template>
   <v-card>
-    <v-toolbar color="primary" dark class="justify-center">jadkajdkasj</v-toolbar>
-    <v-card-text>
-        dadsa
-    </v-card-text>
+    <v-toolbar color="primary" dark class="justify-center">{{pokemon.name | uppercase}}</v-toolbar>
+    <v-card-text>{{urlPokemon}}</v-card-text>
     <v-card-actions>
-        <v-btn small block color="secondary">See more</v-btn>
+      <v-btn small block color="secondary">See more</v-btn>
     </v-card-actions>
   </v-card>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  props: ["pokemon"],
-  name: "PokemonCard"
+  props: ["urlPokemon"],
+  name: "PokemonCard",
+  data() {
+    return {
+      pokemon: {}
+    };
+  },
+  filters: {
+    uppercase: function(value) {
+      if (!value) return "";
+      value = value.toString();
+      return value.toUpperCase();
+    }
+  },
+  created() {
+    this.loadPokemon(this.urlPokemon);
+  },
+  methods: {
+    loadPokemon(resource) {
+      console.log("urlPokemon", this.urlPokemon);
+      console.log(`Querying to resource: ${resource}`);
+
+      // Connect to API: https://pokeapi.co/api/v2
+      axios
+        .get(resource)
+        .then(response => {
+          console.log("\nresponse-->", response);
+          const { data } = response;
+          console.log(data);
+          this.pokemon = data;
+
+          // if (data !== null && data !== undefined) {
+          //   const { results } = data;
+          //   if (results !== null && results !== undefined) {
+          //     console.log("results", results);
+          //     this.pokemonList = results;
+          //   }
+          // }
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  }
 };
 </script>
 
