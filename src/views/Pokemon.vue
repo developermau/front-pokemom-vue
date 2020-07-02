17da7ef3b41a8a3f49552f25fa770da13c6129b2
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2020-07-02 02:16:09 -0400

    ! Trabajando con el pokemon

--- src/views/Pokemon.vue
+++ src/views/Pokemon.vue
@@ -1,18 +1,82 @@
 <template>
-  <v-container fluid>
-    <h1 class="text-center">Pokemon {{id}}</h1>
+  <v-container fluid class="justify-center">
+    <h1 class="text-center">{{pokemon.name | uppercase}}</h1>
     <v-row>
-      <v-col cols="6" md="6">imagen</v-col>
-      <v-col cols="6" md="6">text</v-col>
+      <v-col cols="4" md="4" border>
+        <v-img
+          v-if="pokemon.sprites !== undefined"
+          :src="pokemon.sprites.front_default"
+          aspect-ratio="1"
+          contain
+        ></v-img>
+      </v-col>
+      <v-col cols="8" md="8">
+        <v-list class="transparent">
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>Weight</v-list-item-title>
+              <v-list-item-subtitle>{{pokemon.weight/10}} kg.</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>Height</v-list-item-title>
+              <v-list-item-subtitle>{{pokemon.height*10}} cm.</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   props: ["id"],
-  created(){
-      console.log("this.id-->", this.id)
+  data() {
+    return {
+      pokemon: {}
+    };
+  },
+  filters: {
+    uppercase: function(value) {
+      if (!value) return "";
+      value = value.toString();
+      return value.toUpperCase();
+    }
+  },
+  created() {
+    console.log("this.id-->", this.id);
+    const urlPokemon = `https://pokeapi.co/api/v2/pokemon/${this.id}/`;
+    this.loadPokemon(urlPokemon);
+  },
+  methods: {
+    loadPokemon(resource) {
+      console.log(`Querying to resource: ${resource}`);
+
+      // Connect to API: https://pokeapi.co/api/v2
+      axios
+        .get(resource)
+        .then(response => {
+          const { data } = response;
+          console.log(data);
+
+          if (data !== null && data !== undefined) {
+            this.pokemon = data;
+            const { sprites } = this.pokemon;
+
+            if (sprites !== null && sprites !== undefined) {
+              //   console.log("sprites", sprites);
+              this.pokemonList = sprites;
+            }
+          }
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
   }
 };
 </script>
