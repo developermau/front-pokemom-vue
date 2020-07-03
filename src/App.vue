e261cec4cdbccab030ffd4dd3ac8d7adf58c313e
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2020-07-02 22:12:19 -0400

    + Btn home

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
   <v-app>
     <v-app-bar app color="primary" dark>
       <h2>POKEDEX - "Coursera"</h2>
+      <v-spacer></v-spacer>
+      <v-btn to="/">Home</v-btn>
     </v-app-bar>
     <v-main>
       <v-container fluid>
